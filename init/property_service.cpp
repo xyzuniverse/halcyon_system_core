be9125634582cdd4a5f2e2374ad5489193175444
HeroBuxx <me@herobuxx.me>
2024-09-08 15:45:41 +0000

    init: Do not spoof `ro.debuggable`
    It disables ADB on debugging build
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1387,7 +1387,6 @@ static void SetSafetyNetProps() {
     InitPropertySet("ro.boot.veritymode", "enforcing");
     InitPropertySet("ro.boot.vbmeta.device_state", "locked");
     InitPropertySet("ro.build.type", "user");
-    InitPropertySet("ro.debuggable", "0");
 }
 
 void PropertyInit() {
